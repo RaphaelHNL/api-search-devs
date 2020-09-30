370d23863511d97facdaec0542111a6bdb0a0c72
Raphael Henrique <raohaelhenrique@gmail.com>
2020-09-30 18:48:36 -0300

    adicionando cors

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-var http = require('http');
-const port = process.env.PORT || 3000
+var express = require('express')
+  , cors = require('cors')
+  , app = express();
 
-const devs = require('./devs.json')
-
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify(devs));
+app.options('/products/:id', cors()); // enable pre-flight request for DELETE request 
+app.del('/products/:id', cors(), function (req, res, next) {
+  res.json({ msg: 'This is CORS-enabled for all origins!' });
 });
 
-server.listen(port, () => {
-  console.log(`Server running at port ` + port);
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80');
 });
