540d8f5001b7b16f84372834821c91e195d46ca1
Raphael Henrique <raohaelhenrique@gmail.com>
2020-09-30 18:53:31 -0300

    adicionando cors

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var express = require('express')
 app.options('/products/:id', cors()); // enable pre-flight request for DELETE request 
 app.del('/products/:id', cors(), function (req, res, next) {
   res.json({ msg: 'This is CORS-enabled for all origins!' });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(devs));
 });
 
 app.listen(80, function () {
